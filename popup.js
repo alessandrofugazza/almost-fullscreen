59a76f72fb4a7dbde021bba55ee62e1cc71560bd
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-03-10 20:11:11 +0100

    sort of works

--- popup.js
+++ popup.js
@@ -1,5 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("alertButton").addEventListener("click", function () {
-    alert("Hello, world!");
+document.getElementById("applyStyles").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: toggleVideoStyles,
   });
 });
+
+function toggleVideoStyles() {
+  const video = document.querySelector("video");
+  video.classList.toggle("fuck");
+  const body = document.querySelector("body");
+  body.classList.toggle("fuck-me");
+
+  if (!video) alert("fucking bullshit mate");
+
+  // if (video.classList.contains("almost-fullscreen")) {
+  //   video.classList.remove("almost-fullscreen");
+  // } else {
+  //   video.classList.add("almost-fullscreen");
+  // }
+}
